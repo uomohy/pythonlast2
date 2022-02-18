c0147a1c686f75511b723ae953b4963095d08d72
DESKTOP-MUSED9D\y0206 <y020606@naver.com>
2022-02-18 16:24:21 +0900

    ex01
    변수의 두 개의 값을 서로 바꾸는 예제

--- 2장(변수와계산)/ex01.py
+++ 2장(변수와계산)/ex01.py
@@ -1,14 +1,14 @@
 # 변수의 두 개의 값을 서로 바꾸는 예제
 
-# num1이라는 변수에 100을 저장함.
-num1 = 100
-num2 = 200
-# num1의 데이터 타입을 확인하는 방법은 type함수를 사용하면 됨
-# print(type(num1))
-print("num1 : ", num1, "num2 : ", num2)
+# one이라는 변수에 100을 저장함.
+one = 100
+two = 200
+# one의 데이터 타입을 확인하는 방법은 type함수를 사용하면 됨
+# print(type(one))
+print("one : ", one, "two : ", two)
 
 # 두 개의 변수값을 바꾸기 위해서는 임시변수가 필요함
-temp = num1
-num1 = num2
-num2 = temp
-print("num1 : ", num1, "num2 : ", num2)
+temp = one
+one = two
+two = temp
+print("one : ", one, "two : ", two)
