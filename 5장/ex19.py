2b4d95e4d5f664db6ac8d3fee4ab6f211d69beae
DESKTOP-MUSED9D\y0206 <y020606@naver.com>
2022-02-25 14:32:02 +0900

    ex19
    더블루프를 이용하여
    '*'로 삼각형 출력하는 프로그램

--- 5장/ex19.py
+++ 5장/ex19.py
@@ -1,14 +1,13 @@
-# 더블 루프를 이용하여 아래와 같이 출력하는 프로그램을 작성
+# 더블 루프를 이용하여 아래와 같이 출력하는 프로그램
 # 출력결과
-#     *
-#    ***
-#   *****
-#  *******
-# *********
+#   *
+#  ***
+# *****
+
 # 1. 더블루프 사용
-for i in range(1, 6):
+for i in range(1, 4):
     # 공백을 찍는 내부루프
-    for j in range(5-i):
+    for j in range(3-i):
         print(" ", end="")
     # 별표를 찍는 부분
     # stop 값은 포함이 안됨
@@ -20,34 +19,30 @@ for i in range(1, 6):
 
 # 2. format() 이용 풀이
 print("------------------")
-for i in range(1, 11, 2):
+for i in range(1, 6, 2):
     # 중앙 정렬을 위해서는 ^ 특수문자를 사용하면 됨
-    print("{:^10}".format("*" * i))
+    print("{:^5}".format("*" * i))
 
 # 더블 루프를 이용하여 아래와 같이 출력하는 프로그램을 작성
 # 출력결과
-#     *
-#    ***
-#   *****
-#  *******
-# *********
-# *********
-#  *******
-#   *****
-#    ***
-#     *
+#   *
+#  ***
+# *****
+# *****
+#  ***
+#   *
 print("------------------")
 # 위의 삼각형 부분을 별로 찍는 코드
-for i in range(1, 6):
-    for j in range(5-i):
+for i in range(1, 4):
+    for j in range(3-i):
         print(" ", end="")
     for j in range(1, i*2):
         print("*", end="")
     print("")
 # 아래의 역삼각형 부분을 별로 찍는 코드
-for i in range(5):
+for i in range(4):
     for j in range(i):
         print(" ", end="")
-    for j in range(10, (i*2)+1, -1):
+    for j in range(6, (i*2)+1, -1):
         print("*", end="")
     print("")
